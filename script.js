6e108cb1c6f4d888e3fabb419c34c7fde6809d63
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-03-10 10:40:08 -0600

    Create loader

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const slider = document.querySelector(".slider");
 const sliderLabel = document.querySelector(".slider-label");
 
 let newLabelText = "PLAY";
-let currentLabelText = sliderLabel.textContent; 
+let currentLabelText = sliderLabel.textContent;
 
 //video element
 const video = document.querySelector(".video-container");
 
 sliderContainer.addEventListener("click", () => {
-    slider.classList.toggle("slider-pause"); 
-    sliderLabel.classList.toggle("slider-label-pause");
-    sliderLabel.textContent = newLabelText;
-    newLabelText = currentLabelText;
-    currentLabelText = sliderLabel.textContent;
-    video.paused ? video.play() : video.pause();
+  slider.classList.toggle("slider-pause");
+  sliderLabel.classList.toggle("slider-label-pause");
+  sliderLabel.textContent = newLabelText;
+  newLabelText = currentLabelText;
+  currentLabelText = sliderLabel.textContent;
+  video.paused ? video.play() : video.pause();
+});
+
+//preloader element
+
+const preloader = document.querySelector(".preloader-container");
+
+window.addEventListener("load", () => {
+  preloader.style.display = "none";
 });
